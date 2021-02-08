e64800d24a1926e6880b9a14e4fa7715034c6f04
Ilya <ziluyz@gmail.com>
2021-02-08 18:13:52 +0300

    ArgumentZeroSearcher to mathutils

--- Utils/mathutils.cpp
+++ Utils/mathutils.cpp
@@ -78,4 +78,212 @@ void Runge4::step(double &z0, double z1, double *y) {
     z0 = z1;
 }
 
+bool ArgumentZeroSearcher::BinCoord::operator[](size_t ind) const {
+    if (ind < coord.size()) return coord[ind];
+    else return coord.back();
+}
+
+unsigned int ArgumentZeroSearcher::BinCoord::hash() const {
+    unsigned int res = 0;
+    for (size_t i = coord.size() - 1; i < SIZE_MAX; i--) res = res * 2 + coord[i];
+    return res;
+}
+
+int ArgumentZeroSearcher::BinCoord::compare(const BinCoord &c1, const BinCoord &c2) {
+    auto len = max(c1.coord.size(), c2.coord.size());
+    for (size_t i = 0; i < len; i++) {
+        if (c1[i] > c2[i]) return 1;
+        else if (c1[i] < c2[i]) return -1;
+    }
+    return 0;
+}
+
+ArgumentZeroSearcher::BinCoord ArgumentZeroSearcher::BinCoord::middle(const BinCoord &c1, const BinCoord &c2) {
+    auto c = compare(c1, c2);
+    if (!c) return BinCoord(c1);
+    auto smaller = &c1;
+    if (c == 1) smaller = &c2;
+    BinCoord res(*smaller);
+    if (res.coord.size() > 1) {
+        res.coord.pop_back();
+        res.coord.pop_back();
+        res.coord.push_back(true);
+        res.coord.push_back(false);
+    }
+    auto len = max(c1.coord.size(), c2.coord.size());
+    while (res.coord.size() < len)
+        res.coord.push_back(false);
+    res.coord.push_back(true);
+    return res;
+}
+
+void ArgumentZeroSearcher::Point::operator=(const Point &p) {
+    bx = p.bx;
+    by = p.by;
+    inx = p.x;
+    iny = p.y;
+}
+
+unsigned long long ArgumentZeroSearcher::Point::hash() const {
+    union mapper {
+        unsigned long long res;
+        unsigned int parts[2];
+    } res;
+    res.parts[0] = bx.hash();
+    res.parts[1] = by.hash();
+    return res.res;
+}
+
+void ArgumentZeroSearcher::Point::middle(const ArgumentZeroSearcher::Point &p1, const ArgumentZeroSearcher::Point &p2, ArgumentZeroSearcher::Point &res) {
+    res.inx = (p1.x + p2.x) / 2;
+    res.iny = (p1.y + p2.y) / 2;
+    res.bx = BinCoord::middle(p1.bx, p2.bx);
+    res.by = BinCoord::middle(p1.by, p2.by);
+}
+
+ArgumentZeroSearcher::Cell::Cell(const ArgumentZeroSearcher::Point &lb, const ArgumentZeroSearcher::Point &rt) : points() {
+    points[0] = lb;
+    points[2] = rt;
+    Point &rb = points[1];
+    rb = Point(rt.x, lb.y, false, false);
+    rb.bx = rt.bx;
+    rb.by = lb.by;
+    Point &lt = points[3];
+    lt = Point(lb.x, rt.y, false, false);
+    lt.bx = lb.bx;
+    lt.by = rt.by;
+}
+
+ArgumentZeroSearcher::Edge ArgumentZeroSearcher::Cell::edge(size_t ind) const {
+    switch (ind) {
+    case 0:
+        return Edge{points[0], points[1]};
+    case 1:
+        return Edge{points[1], points[2]};
+    case 2:
+        return Edge{points[2], points[3]};
+    case 3:
+        return Edge{points[3], points[0]};
+    default:
+        throw 1;// RuntimeException("Bad edge index in ZeroSearcher Cell::edge");
+    }
+}
+
+ArgumentZeroSearcher::Cell ArgumentZeroSearcher::Cell::cell(size_t ind) const {
+    Point m, m2;
+    switch (ind) {
+    case 0:
+        Point::middle(points[0], points[2], m);
+        return Cell(points[0], m);
+    case 1:
+        Point::middle(points[0], points[1], m);
+        Point::middle(points[1], points[2], m2);
+        return Cell(m, m2);
+    case 2:
+        Point::middle(points[0], points[2], m);
+        return Cell(m, points[2]);
+    case 3:
+        Point::middle(points[0], points[3], m);
+        Point::middle(points[2], points[3], m2);
+        return Cell(m, m2);
+    default:
+        throw 1;//RuntimeException("Bad cell index in ZeroSearcher Cell::cell");
+    }
+}
+
+double ArgumentZeroSearcher::eval(const ArgumentZeroSearcher::Point &p, bool lookHash) {
+    auto hash = p.hash();
+    if (lookHash) {
+        if (mem.count(hash)) return mem[hash];
+    }
+    neval++;
+    auto res = func(p);
+    if (res.real() < acc || neval > 3000) {
+        found = true;
+        zsol = p;
+    }
+    if (lookHash) mem[hash] = res.imag();
+    return res.imag();
+}
+
+bool ArgumentZeroSearcher::addPhaseOnEdge(ArgumentZeroSearcher::Edge e, double &phase, bool lookHash) {
+    double ph1 = eval(e.p1, lookHash);
+    if (found) return true;
+    double ph2 = eval(e.p2, lookHash);
+    if (found) return true;
+    double tph = ph2 - ph1;
+    if (tph > M_PI) tph -= 2 * M_PI;
+    if (tph < -M_PI) tph += 2 * M_PI;
+    if (abs(tph) > 1) {
+        Point m;
+        Point::middle(e.p1, e.p2, m);
+        if (m.order() > 30) {
+            Point p1(e.p1.x, e.p1.y, false, false);
+            Point p2(e.p2.x, e.p2.y, true, true);
+            if (addPhaseOnEdge(Edge{p1, p2}, phase, false)) return true;
+        } else {
+            if (addPhaseOnEdge(Edge{e.p1, m}, phase, lookHash)) return true;
+            if (addPhaseOnEdge(Edge{m, e.p2}, phase, lookHash)) return true;
+        }
+    } else
+        phase += tph;
+    return false;
+}
+
+bool ArgumentZeroSearcher::checkCell(const ArgumentZeroSearcher::Cell &c, bool &result) {
+    double phase = 0;
+    for (size_t i = 0; i < 4; i++)
+        if (addPhaseOnEdge(c.edge(i), phase)) return true;
+    result = abs(phase) > 1;
+    return false;
+}
+
+bool ArgumentZeroSearcher::checkCellHard(size_t level, const ArgumentZeroSearcher::Cell &c, bool &result) {
+    if (level == 0) return checkCell(c, result);
+    for (size_t i = 0; i < 4; i++) {
+        auto cell = c.cell(i);
+        if (checkCellHard(level - 1, cell, result)) return true;
+        if (result) return false;
+    }
+    return false;
+}
+
+void ArgumentZeroSearcher::iterate(size_t lev, const ArgumentZeroSearcher::Cell &c) {
+    if (lev == 30) throw 1;//RuntimeException("Max level in ZeroSearcher reached");
+    bool containZero;
+    for (size_t hardness = 0; hardness < 3; hardness++) {
+        if (hardness + lev == 30) break;
+        for (size_t i = 0; i < 4; i++) {
+            auto cell = c.cell(i);
+            if (checkCellHard(hardness, cell, containZero)) return;
+            if (containZero) {
+                iterate(lev + 1, cell);
+                if (found) break;
+            }
+        }
+        if (found) break;
+    }
+    if (!(lev || found)) throw 1;//RuntimeException("ZeroSearcher error");
+}
+
+bool ArgumentZeroSearcher::findZero() {
+    bool exists = false;
+    Cell maincell(Point(minmax.xmin, minmax.ymin, false, false), Point(minmax.xmax, minmax.ymax, true, true));
+    for (size_t i = 0; i < 2; i++) {
+        if (checkCellHard(i, maincell, exists)) return true;
+        if (exists) break;
+    }
+    if (!exists) return false;
+    iterate(0, maincell);
+    return true;
+}
+
+bool ArgumentZeroSearcher::find(const function<CMPLX(CMPLX)> &func, ArgumentZeroSearcher::Rect minmax, double acc, CMPLX &z) {
+    ArgumentZeroSearcher sr(func, minmax, acc);
+    auto ok = sr.findZero();
+    if (ok) z = sr.zsol;
+    //cout << "Number of calls: " << sr.neval << endl;
+    return ok;
+}
+
 }
--- Utils/mathutils.h
+++ Utils/mathutils.h
@@ -1,5 +1,10 @@
 #include <functional>
 #include <vector>
+#include <unordered_map>
+#include <complex>
+#include <cmath>
+
+#define CMPLX std::complex<double>
 
 namespace mut {
 
@@ -19,6 +24,82 @@ class Runge4 {
     public:
         Runge4(size_t n, std::function<int(double, const double*, double*, void*)> rights, void *pP);
         void step(double &z0, double z1, double *y);
+};
+
+class ArgumentZeroSearcher {
+public:
+    struct Rect {
+        double xmin, xmax, ymin, ymax;
+    };
+
+private:
+    class BinCoord {
+    private:
+        std::vector<bool> coord;
+        bool operator[](size_t ind) const;
+
+    public:
+        BinCoord(bool val) {coord.push_back(val);}
+        unsigned int hash() const;
+        size_t order() const {return coord.size();}
+        static int compare(const BinCoord &c1, const BinCoord &c2);
+        static BinCoord middle(const BinCoord &c1, const BinCoord &c2);
+    };
+
+    class Cell;
+
+    class Point {
+        friend class Cell;
+
+    private:
+        BinCoord bx, by;
+        double inx, iny;
+
+    public:
+        const double &x = inx, &y = iny;
+        Point() : bx(false), by(false), inx(0), iny(0) {}
+        Point(double x, double y, bool bx, bool by) : bx(bx), by(by), inx(x), iny(y) {}
+        Point(const Point &p) = delete;
+        void operator=(const Point &p);
+        operator CMPLX() const { return CMPLX{inx, iny}; }
+        unsigned long long hash() const;
+        size_t order() const { return std::max(bx.order(), by.order());}
+        static void middle(const Point &p1, const Point &p2, Point &res);
+    };
+
+    class Edge {
+    public:
+        const Point &p1, &p2;
+    };
+
+    class Cell {
+    private:
+        Point points[4];
+
+    public:
+        Cell(const Point &lb, const Point &rt);
+        Cell(const Cell &c) {for (size_t i = 0; i < 4; i++) points[i] = c.points[i];}
+        Edge edge(size_t ind) const;
+        Cell cell(size_t ind) const;
+    };
+
+    const std::function<CMPLX(CMPLX)> &func;
+    Rect minmax;
+    double acc;
+    bool found = false;
+    CMPLX zsol;
+    int neval = 0;
+    std::unordered_map<unsigned long long, double> mem;
+    ArgumentZeroSearcher(const std::function<CMPLX(CMPLX)> &func, Rect minmax, double acc) : func(func), minmax(minmax), acc(acc) {}
+    double eval(const Point &p, bool lookHash);
+    bool addPhaseOnEdge(Edge e, double &phase, bool lookHash = true);
+    bool checkCell(const Cell &c, bool &result);
+    bool checkCellHard(size_t level, const Cell &c, bool &result);
+    void iterate(size_t lev, const Cell &c);
+    bool findZero();
+
+public:
+    static bool find(const std::function<CMPLX(CMPLX)> &func, Rect minmax, double acc, CMPLX &z);
 };
 
 }
