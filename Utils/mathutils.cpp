c5983d0d31b059ef3577a78d0d6235848ff797b4
Ilya <ziluyz@gmail.com>
2021-02-08 15:25:45 +0300

    Runge4 to mathutils

--- Utils/mathutils.cpp
+++ Utils/mathutils.cpp
@@ -2,8 +2,10 @@
 
 using namespace std;
 
+namespace mut {
+
 //Find zero by bisection
-bool mut::findRoot(const function<double(double)> &fcn, double xMin, double xMax, double absErr, double &res)
+bool findRoot(const function<double(double)> &fcn, double xMin, double xMax, double absErr, double &res)
 {
     if (xMin >= xMax)
         throw 1; //RuntimeException("xMin greater than xMax in findRoot()");
@@ -31,7 +33,7 @@ bool mut::findRoot(const function<double(double)> &fcn, double xMin, double xMax
 }
 
 //Find a sequence of roots
-vector<double> mut::findRootsSequence(const function<double(double)> &fcn, double xStart, double minRootDist, int n)
+vector<double> findRootsSequence(const function<double(double)> &fcn, double xStart, double minRootDist, int n)
 {
     std::vector<double> res;
     double xMin = xStart;
@@ -50,4 +52,30 @@ vector<double> mut::findRootsSequence(const function<double(double)> &fcn, doubl
         xMin = res.back() + xStep;
     }
     return res;
+}
+
+void Runge4::vpav(double *v1, double a, vector<double> &v2, vector<double> &res) {
+    for (size_t i = 0; i < Neq; i++) res[i] = v1[i] + a * v2[i];
+}
+
+Runge4::Runge4(size_t n, function<int(double, const double*, double*, void*)> rights, void *pP) : Neq(n), rights(rights), pP(pP) {
+    for (int i = 0; i < 5; i++) kys.emplace_back(Neq);
+}
+
+void Runge4::step(double &z0, double z1, double *y) {
+    auto zm = (z0 + z1) / 2;
+    auto h = z1 - z0;
+    auto hd2 = h / 2;
+    auto hd6 = h / 6;
+    rights(z0, y, kys[1].data(), pP);
+    vpav(y, hd2, kys[1], kys[0]);
+    rights(zm, kys[0].data(), kys[2].data(), pP);
+    vpav(y, hd2, kys[2], kys[0]);
+    rights(zm, kys[0].data(), kys[3].data(), pP);
+    vpav(y, h, kys[3], kys[0]);
+    rights(z1, kys[0].data(), kys[4].data(), pP);
+    for (size_t i = 0; i < Neq; i++) y[i] += hd6 * (kys[1][i] + 2 * kys[2][i] + 2 * kys[3][i] + kys[4][i]);
+    z0 = z1;
+}
+
 }
--- Utils/mathutils.h
+++ Utils/mathutils.h
@@ -1,13 +1,24 @@
 #include <functional>
 #include <vector>
 
-class mut {
-public:
+namespace mut {
 
 //Find zero by bisections
-static bool findRoot(const std::function<double(double)> &fcn, double xMin, double xMax, double absErr, double &res);
+bool findRoot(const std::function<double(double)> &fcn, double xMin, double xMax, double absErr, double &res);
 
 //Find a sequence of roots
-static std::vector<double> findRootsSequence(const std::function<double(double)> &fcn, double xStart, double minRootDist, int n);
+std::vector<double> findRootsSequence(const std::function<double(double)> &fcn, double xStart, double minRootDist, int n);
 
+class Runge4 {
+    private:
+        std::vector<std::vector<double>> kys;
+        size_t Neq;
+        const std::function<int(double, const double*, double*, void*)> rights;
+        void *pP;
+        void vpav(double *, double, std::vector<double> &, std::vector<double> &);
+    public:
+        Runge4(size_t n, std::function<int(double, const double*, double*, void*)> rights, void *pP);
+        void step(double &z0, double z1, double *y);
 };
+
+}
